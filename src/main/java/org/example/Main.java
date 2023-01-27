35399d665c6a70830238e87d9ae881ed04624502
luizfellipes <luiz.13.silva@gmail.com>
2023-01-27 11:40:43 -0400

    Conceitos de Polimorfismo

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,21 @@
+package org.example;
+
+public class Main {
+    public static void main(String[] args) {
+        Mamifero mamifero = new Mamifero();
+        Reptil reptil = new Reptil();
+        Peixe peixe = new Peixe();
+        Ave ave = new Ave();
+
+        mamifero.setPeso(85.3f);
+        mamifero.setIdade(2);
+
+
+
+
+
+
+
+
+    }
+}
